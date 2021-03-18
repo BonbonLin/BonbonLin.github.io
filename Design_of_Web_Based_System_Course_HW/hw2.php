d0f9b2f952acd63d7d57bb6f2bb692d07f46d9ee
Bonbon <t106300217@ntut.org.tw>
2021-03-18 11:38:01 +0800

    ok

--- Design_of_Web_Based_System_Course_HW/hw2.php
+++ Design_of_Web_Based_System_Course_HW/hw2.php
@@ -7,21 +7,22 @@
   <body>
     <?php
     function num_eval($a){
-        if(is_int($a)){
-            if ($a >= 0 && $a<10){
-                echo "$a 是整數個位數";
-                echo "<br />";
-            }
-            else{
-                echo "$a 是整數，但不是個位數";
-                echo "<br />";
-            }
-          }
-          elseif(is_float($a)){
-              echo "$a 是小數";
+      if(is_int($a)){
+          if ($a >= 0 && $a<10){
+              echo "$a 是整數個位數";
               echo "<br />";
           }
+          else{
+              echo "$a 是整數，但不是個位數";
+              echo "<br />";
+          }
+        }
+      elseif(is_float($a)){
+          echo "$a 是小數";
+          echo "<br />";
+        }
     }
+
     num_eval(1);
     num_eval(1.1);
     num_eval(11);
