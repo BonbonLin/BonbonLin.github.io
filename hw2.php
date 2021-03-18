debece57d381c7e0ff515fe3dde22627a6f35073
Bonbon <t106300217@ntut.org.tw>
2021-03-18 10:58:47 +0800

    upload

---
+++ hw2.php
@@ -0,0 +1,30 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>作業2</title>  
+  </head>
+  
+  <body>
+    <?php
+    function num_eval($a){
+        if(is_int($a)){
+            if ($a >= 0 && $a<10){
+                echo "$a 是整數個位數";
+                echo "<br />";
+            }
+            else{
+                echo "$a 是整數，但不是個位數";
+                echo "<br />";
+            }
+          }
+          elseif(is_float($a)){
+              echo "$a 是小數";
+              echo "<br />";
+          }
+    }
+    num_eval(1);
+    num_eval(1.1);
+    num_eval(11);
+    ?>
+  </body>
+</html>
